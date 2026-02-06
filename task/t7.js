ef5760437e1a57e334552c18b111d0070dec47ae
Gabriele Viganò <infogabrielevigano@gmail.com>
2026-02-06 14:02:30 +0100

    feat: finish task 07

--- task/t7.js
+++ task/t7.js
@@ -1,3 +1,18 @@
 const prompt = require("prompt-sync")();
 
+const username = prompt("Enter your username: ");
 
+if (username == "Admin") {
+  const password = prompt("Enter your password: ");
+  if (password == "TheMaster") {
+    console.log("Welcome!");
+  } else if (password == "" || password == null) {
+    console.log("Canceled");
+  } else {
+    console.log("Wrong password");
+  }
+} else if (username == "" || username == null) {
+  console.log("Canceled");
+} else {
+  console.log("I don't know you");
+}
