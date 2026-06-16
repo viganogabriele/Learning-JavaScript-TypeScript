b3487fb566f52026faa1e2e28674ff3bd7cf8c0b
Gabriele Viganò <infogabrielevigano@gmail.com>
2026-06-16 15:26:29 +0200

    feat: enhance coin toss animation and update image alt text

--- 01-flip-the-coin/script.js
+++ 01-flip-the-coin/script.js
@@ -3,20 +3,31 @@ const resultContainer = document.querySelector(".result-container");
 const imgToss = document.querySelector(".img--toss");
 
 btnToss.addEventListener("click", () => {
-	resultContainer.innerHTML = "";
-	let result = document.createElement("p");
+	imgToss.classList.remove("animation-toss");
+	void imgToss.offsetWidth;
+	imgToss.classList.add("animation-toss");
+
+	// Calculate the result
 	let random = Math.random() < 0.5 ? "Head" : "Tail";
 
-	result.classList.add("text-result");
-	result.textContent = random;
+	// Delay the visual refresh by 200 milliseconds
+	setTimeout(() => {
+		resultContainer.innerHTML = "";
+		let result = document.createElement("p");
 
-	if (random === "Head") {
-		imgToss.setAttribute("src", "assets/coin-head.png");
-		imgToss.setAttribute("alt", "head");
-	} else {
-		imgToss.setAttribute("src", "assets/coin-tail.png");
-		imgToss.setAttribute("alt", "tail");
-	}
+		result.classList.add("text-result");
+		result.textContent = random;
 
-	resultContainer.appendChild(result);
+		if (random === "Head") {
+			imgToss.setAttribute("src", "assets/coin-head.png");
+			imgToss.setAttribute("alt", "head");
+		} else {
+			imgToss.setAttribute("src", "assets/coin-tail.png");
+			imgToss.setAttribute("alt", "tail");
+		}
+
+		resultContainer.appendChild(result);
+	}, 200);
 });
+
+imgToss.addEventListener("click", () => btnToss.click());
