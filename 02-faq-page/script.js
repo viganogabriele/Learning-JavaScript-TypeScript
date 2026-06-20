af46a07045cd35d423b1e98d725b8bf5f51ded3d
Gabriele Viganò <infogabrielevigano@gmail.com>
2026-06-20 15:16:09 +0200

    style: implement FAQ item toggle functionality with active state management

--- 02-faq-page/script.js
+++ 02-faq-page/script.js
@@ -1,8 +1,12 @@
 const filterButtons = document.querySelectorAll(".faq-filters__btn");
 const faqPanels = document.querySelectorAll(".faq-panel");
+let openedFaqs = [];
+const MAX_OPENED = 2;
 
 filterButtons.forEach((button) => {
   button.addEventListener("click", () => {
+    openedFaqs.forEach((item) => item.classList.remove("faq-item--active"));
+
     document
       .querySelector(".faq-filters__btn--active")
       .classList.remove("faq-filters__btn--active");
@@ -16,5 +20,40 @@ filterButtons.forEach((button) => {
       .classList.remove("faq-panel--active");
 
     document.querySelector(`#${targetID}`).classList.add("faq-panel--active");
+
+    // Clean openedFaqs before switching filter
+
+    initPanelFaqs(targetID);
   });
 });
+
+const faqItem = document.querySelectorAll(".faq-item");
+const faqQuestion = document.querySelectorAll(".faq-item__question");
+
+faqQuestion.forEach((button) => {
+  button.addEventListener("click", () => {
+    const parentItem = button.closest(".faq-item");
+
+    if (parentItem.classList.contains("faq-item--active")) {
+      parentItem.classList.remove("faq-item--active");
+
+      openedFaqs = openedFaqs.filter((item) => item !== parentItem);
+    } else {
+      parentItem.classList.add("faq-item--active");
+      openedFaqs.push(parentItem);
+    }
+
+    if (openedFaqs.length > MAX_OPENED) {
+      const itemRemove = openedFaqs.shift();
+      itemRemove.classList.remove("faq-item--active");
+    }
+  });
+});
+
+function initPanelFaqs(panelID) {
+  openedFaqs = [];
+}
+
+// Initialize openedFaqs and faq-item to show
+const initialPanelId = document.querySelector(".faq-panel--active").id;
+initPanelFaqs(initialPanelId);
